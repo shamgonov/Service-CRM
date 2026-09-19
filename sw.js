aa11a5f41c98ef8554bedd3ba48d4b16b1919b7e
shamgonov <shamgoncv@gmail.com>
2026-09-19 21:05:44 +0500

    деплой №108 v2.2.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='2.2.6';const CACHE='crm-'+VERSION;const CORE=['./','index.html','manifest.json','icon.png','icon-192.png','version.json'];
+const VERSION='2.2.7';const CACHE='crm-'+VERSION;const CORE=['./','index.html','manifest.json','icon.png','icon-192.png','version.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url);
